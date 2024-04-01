90a00581f7c6d4a4e57f7afc813aab8ff3225204
Danila Borovskiy <borovski.danila00@gmail.com>
2024-04-02 01:11:10 +0300

    0.5.0

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
-import UserDbService from "@/modules/user/db";
 
 export async function GET() {
-  return NextResponse.json({ users: await UserDbService.getAllUsers() });
+  return NextResponse.json({ users: [] });
 }
