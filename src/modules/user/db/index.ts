cd3355f800ed5275e6692aa5230042226c592759
Danila Borovskiy <borovski.danila00@gmail.com>
2024-04-02 01:04:27 +0300

    0.5.0

--- src/modules/user/db/index.ts
+++ src/modules/user/db/index.ts
@@ -1,15 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-// eslint-disable-next-line
-type CustomAny = any;
-
 const prisma = new PrismaClient();
 
 class UserDbService {
   static getAllUsers() {
     return prisma.user
       .findMany()
-      .catch((error: CustomAny) => console.log(error));
+      .then(() => [{}])
+      .catch(() => [null]);
   }
 }
 
