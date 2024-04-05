3f11c922aaa604a21356c1ccea2c7c8a29ea1dde
Danila Borovskiy <borovski.danila00@gmail.com>
2024-04-05 22:55:14 +0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,6 @@ export default function Home() {
             Welcome to My Course Project
           </h1>
 
-          <h1>DB: {process.env.SERVER_ONLY_DATABASE_URL}</h1>
-
-          <h1>URL1: {process.env.NEXT_PUBLIC_API_URL}</h1>
-          <h1>URL2: {process.env.API_URL}</h1>
-
           <p className="mt-2 text-lg text-gray-600">
             This is my course project where I showcase my work and report.
           </p>
